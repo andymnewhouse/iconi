378245b8dc853578cd50c10cc7bf71704bb0e78c
Andy Newhouse <hi@andymnewhouse.me>
2023-09-15 11:04:10 -0500

    Bring back Unit test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
