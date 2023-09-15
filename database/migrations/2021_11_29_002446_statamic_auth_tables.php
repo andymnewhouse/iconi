3006ad114000d46baf76d3e72f56105264884a5a
Shift <shift@laravelshift.com>
2023-09-15 14:03:19 +0000

    Adopt anonymous migrations

--- database/migrations/2021_11_29_002446_statamic_auth_tables.php
+++ database/migrations/2021_11_29_002446_statamic_auth_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class StatamicAuthTables extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -58,4 +58,4 @@ class StatamicAuthTables extends Migration
 
         Schema::dropIfExists('password_activations');
     }
-}
+};
